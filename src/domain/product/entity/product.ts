5bdabbb948add04d4ea0c913a56a1ee172f66655
Vyctor <dev.vyctor@gmail.com>
2022-09-05 01:15:07 -0300

    notification pattern para Product

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,18 +1,17 @@
+import { Entity } from '../../shared/entity/entity.abstract';
+import { NotificationError } from '../../shared/notification/notification.error';
 import ProductInterface from './product.interface';
-export default class Product implements ProductInterface {
-  private _id: string;
+
+export default class Product extends Entity implements ProductInterface {
   private _name: string;
   private _price: number;
 
   constructor(id: string, name: string, price: number) {
+    super();
     this._id = id;
     this._name = name;
     this._price = price;
     this.validate();
-  }
-
-  get id(): string {
-    return this._id;
   }
 
   get price(): number {
@@ -25,33 +24,48 @@ export default class Product implements ProductInterface {
 
   public changeName(name: string): void {
     this._name = name;
+    this.validate();
   }
 
   public changePrice(price: number): void {
     this._price = price;
+    this.validate();
   }
 
   private validate(): void {
     this.validateId();
     this.validateName();
     this.validatePrice();
+
+    if (this.notification.hasErrors()) {
+      throw new NotificationError(this.notification.getErrors());
+    }
   }
 
   private validateId(): void {
     if (!this._id) {
-      throw new Error('Id is required');
+      this.notification.addError({
+        message: 'Id is required',
+        context: 'Product',
+      });
     }
   }
 
   private validateName(): void {
     if (!this._name) {
-      throw new Error('Name is required');
+      this.notification.addError({
+        message: 'Name is required',
+        context: 'Product',
+      });
     }
   }
 
   private validatePrice(): void {
     if (!this._price || this.price <= 0) {
-      throw new Error('Price is required and need to be greater than 0');
+      this.notification.addError({
+        message: 'Price is required and need to be greater than 0',
+        context: 'Product',
+      });
     }
   }
 }
