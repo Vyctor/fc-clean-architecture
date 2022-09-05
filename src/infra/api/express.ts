373fb6f8cf9d9543d4fce69a1b0b33dc180c774e
Vyctor <dev.vyctor@gmail.com>
2022-09-04 23:48:57 -0300

    endpoint para product

--- src/infra/api/express.ts
+++ src/infra/api/express.ts
@@ -3,12 +3,14 @@ import { Sequelize } from 'sequelize-typescript';
 import CustomerModel from '../customer/repository/sequelize/customer.model';
 import ProductModel from '../product/repository/sequelize/product.model';
 import { customerRoute } from './routes/customer.route';
+import { productRoute } from './routes/product.route';
 
 export const app: Express = express();
 export let sequelize: Sequelize;
 
 app.use(express.json());
 app.use('/customer', customerRoute);
+app.use('/product', productRoute);
 
 async function setupDatabase(): Promise<void> {
   sequelize = new Sequelize({
